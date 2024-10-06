ec4afac081aa96e1cfa00935fa39cf464bbe8419
minyeong <minyk0512@gmail.com>
2024-10-06 18:52:22 +0900

    :sparkles: feat: 전체 로딩 ui 추가

---
+++ src/app/loading.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Image from 'next/image';
+import rabbit from '../public/rabbit.gif';
+
+const Loading = () => {
+  return (
+    <div className='flex flex-col gap-5 justify-center items-center w-full min-h-screen'>
+      <p className='text-white text-3xl'>잠시만 기다려주세요!</p>
+      <Image
+        src={rabbit}
+        alt='Loading'
+        width={400}
+        height={400}
+      />
+    </div>
+  );
+};
+
+export default Loading;
