09bd7ee8ed43a1c9fa8a6950be6c8fc05673d26c
minyeong <minyk0512@gmail.com>
2024-10-06 21:49:56 +0900

    :sparkles: feat: Global Error 추가

---
+++ src/app/global-error.tsx
@@ -0,0 +1,15 @@
+'use client';
+import React from 'react';
+
+const GlobalError = ({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) => {
+  return (
+    <html>
+      <body>
+        <h2>에러가 발생했습니다.</h2>
+        <button onClick={() => reset()}>다시 시도하기</button>
+      </body>
+    </html>
+  );
+};
+
+export default GlobalError;
